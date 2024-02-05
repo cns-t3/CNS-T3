ab6d4038825091da4df1219dfcec10d446ff09f8
meowzybell <mypeh.2021@scis.smu.edu.sg>
2024-02-05 10:55:27 +0800

    SCRUM-11 New background

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors:{
+        'custom-beige': '#F9F9F7',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
