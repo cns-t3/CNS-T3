6df697aff110feb3e2f83d97054f8b62bab93e4c
desireely <105425207+desireely@users.noreply.github.com>
2024-02-06 19:29:44 +0800

    fix ui and added test

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,36 +1,73 @@
 import Image from "next/image";
 
+const formatDate = (inputDate) => {
+  const dateObj = new Date(inputDate);
+
+  if (isNaN(dateObj.getTime())) {
+    return "Invalid Date";
+  }
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const day = dateObj.getDate();
+  const month = months[dateObj.getMonth()];
+  const year = dateObj.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
+
 const Profile = ({ profileDetails }) => {
+  const formattedDate =
+    profileDetails.dob !== "" ? formatDate(profileDetails.dob) : "";
+
   return (
-    <div className="col-span-1 bg-beige p-8">
-      <div className="grid grid-cols-2 gap-5 ">
-        <div className="w-48 h-96">
-          <Image
+    <div id="profile-container" className="col-span-1 p-8">
+      <div className="grid grid-cols-2 gap-1 ">
+        <div id="profile-image-container" className="">
+          <img
             src={profileDetails.img_url}
             alt="Profile image"
-            width={150}
-            height={300}
-            className="object-cover w-full h-full"
+            className="object-cover w-3/4 h-full"
           />
         </div>
         <div>
-          <h2 className="text-xl font-bold text-gray-900 my-1">
+          <h2
+            id="profile-name"
+            className="text-xl font-bold text-gray-900 my-1"
+          >
             {profileDetails.name}
           </h2>
-          <p className="text-gray-500 text-sm">
+          <p id="profile-occupation" className="text-sm mb-5">
             {profileDetails.occupation}, at {profileDetails.company}
           </p>
           {profileDetails.dob !== "" && (
-            <p className="text-gray-500 text-sm">Born: {profileDetails.dob}</p>
+            <p id="profile-dob" className="text-gray-500 text-sm">
+              Born: {formattedDate}
+            </p>
           )}
           {profileDetails.nationality !== "" && (
-            <p className="text-gray-500 text-sm">
+            <p id="profile-nationality" className="text-gray-500 text-sm">
               Nationality: {profileDetails.nationality}
             </p>
           )}
         </div>
       </div>
-      <div className="text-sm mt-5">{profileDetails.description}</div>
+      <div id="profile-description" className="text-sm mt-5">
+        {profileDetails.description}
+      </div>
     </div>
   );
 };
