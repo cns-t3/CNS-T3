c36365836884b005c3f066254873675570d6949d
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-02-06 15:12:26 +0800

    Added CD basic workflow

---
+++ backend/api/newsAPI/main.py
@@ -0,0 +1,45 @@
+from typing import List
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from openai import OpenAI
+from summarise_data import get_summarised_news_articles
+
+
+load_dotenv()
+app = FastAPI()
+openAI_client = OpenAI()
+
+
+class NewsArticle(BaseModel):
+    news_id: int
+    title: str
+    content: str
+    publisher: str
+    publishedAt: str
+    image_url: str = ""
+    source_url: str
+    risk_rating: str
+    summary: str
+    score: int
+    tag: str
+
+@app.get("/health", tags=["health"], summary="Check health status", description="Returns the health status of the service")
+async def health_check():
+    return {"status": "healthy"}
+
+@app.get(
+    "/news/{search_query}",
+    response_model=List[NewsArticle],
+    tags=["news"],
+    summary="Get news articles",
+    description="Returns an array of news articles",
+)
+async def get_news_articles(search_query: str):
+    try:
+        return get_summarised_news_articles(search_query)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
+        )
