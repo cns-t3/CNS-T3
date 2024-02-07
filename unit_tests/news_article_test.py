f54a5a8facf025532eb2549ec2d42bf735715503
desireely <105425207+desireely@users.noreply.github.com>
2024-02-07 21:43:34 +0800

    edit test

--- unit_tests/news_article_test.py
+++ unit_tests/news_article_test.py
@@ -2,48 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 driver = webdriver.Chrome()
-driver.maximize_window()
 driver.implicitly_wait(10)
 
 # Open the React application URL
-driver.get("http://localhost:3001/search/tim%20cook")
-time.sleep(1)
+driver.get("http://localhost:3000/search/tim%20cook")
 
 try:
     # Wait for the news article container to be present and displayed
     news_article_container = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.ID, "news-article-container"))
     )
-    assert (
-        news_article_container.is_displayed()
-    ), "News article container is not displayed"
+    assert news_article_container.is_displayed(), "News article container is not displayed"
 
-    assert (
-        WebDriverWait(driver, 10)
-        .until(EC.presence_of_element_located((By.ID, "article-publisher")))
-        .is_displayed()
-    ), "Article publisher is not displayed"
+    # Validate the presence of each element
 
-    assert (
-        WebDriverWait(driver, 10)
-        .until(EC.presence_of_element_located((By.ID, "article-title")))
-        .is_displayed()
-    ), "Article title is not displayed"
+    assert WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "article-publisher"))
+    ).is_displayed(), "Article publisher is not displayed"
 
-    assert (
-        WebDriverWait(driver, 10)
-        .until(EC.presence_of_element_located((By.ID, "article-tag-hidden")))
-        .is_displayed()
-    ), "Article tag (hidden) is not displayed"
+    assert WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "article-title"))
+    ).is_displayed(), "Article title is not displayed"
 
-    assert (
-        WebDriverWait(driver, 10)
-        .until(EC.presence_of_element_located((By.ID, "article-summary")))
-        .is_displayed()
-    ), "Article summary is not displayed"
+    assert WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "article-tag-hidden"))
+    ).is_displayed(), "Article tag (hidden) is not displayed"
+
+    assert WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "article-summary"))
+    ).is_displayed(), "Article summary is not displayed"
 
     print("Presence of each element in the NewsArticle component is validated.")
 
