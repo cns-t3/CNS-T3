5af4854bab77a8a63c9077bad130fc039b031271
De Han Chia <96176180+DHChia@users.noreply.github.com>
2024-02-19 23:58:37 +0800

    feature/SCRUM-24-Set-Up-Database (#8)
    * Initialising database structure
    
    Adding database, model, schema and crud.py files
    
    * Added alembic and first migration script
    
    * Created tables in Azure SQL DB via Alembic migration script
    
    Modified env.py and alembic.ini to add DB endpoint
    
    Pushed alembic head to create database tables

---
+++ alembic/versions/bb8d4633fee8_first_migration.py
@@ -0,0 +1,52 @@
+"""First Migration
+
+Revision ID: bb8d4633fee8
+Revises: 
+Create Date: 2024-02-06 22:58:07.716398
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
+
+# revision identifiers, used by Alembic.
+revision: str = 'bb8d4633fee8'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'person',
+        Column('PersonID', Integer, primary_key=True, autoincrement=True),
+        Column('Name', String(50), nullable=False),
+        Column('Occupation', String(25), nullable=False),
+        Column('DoB', Date, nullable=False),
+        Column('Nationality', String(25), nullable=False),
+        Column('Description', String(255), nullable=False),
+        Column('CountryOfResidency', String(50), nullable=False),
+        Column('PEPStatus', Boolean),
+        Column('SourceOfWealth', String(25)),
+        Column('ImgURL', String(255))
+    )
+
+    op.create_table(
+        'company',
+        Column('CompanyID', Integer, primary_key=True, autoincrement=True),
+        Column('Name', String(50), nullable=False),
+        Column('Industry', String(25), nullable=False),
+        Column('CountryofHeadquarters', String(50), nullable=False),
+        Column('Type', String(25), nullable=False),
+    )
+
+    op.create_table(
+        'person_company',
+        Column('PersonID', Integer, ForeignKey('person.PersonID'), primary_key=True),
+        Column('CompanyID', Integer, ForeignKey('company.CompanyID'), primary_key=True),
+        Column('Role', String(25), nullable=False),
+    )
+
+def downgrade() -> None:
+    pass
