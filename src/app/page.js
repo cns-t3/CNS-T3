f377abceb07e78af7c5ea9ecdda7f807ce090311
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-02-21 01:04:56 +0800

    Test AKS without secrets

--- src/app/page.js
+++ src/app/page.js
@@ -6,23 +6,23 @@ import axios from "axios";
 export default function Home() {
   const [data1, setData] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://10.0.142.62:8000/news/tim%20cook")
-      .then((response) => {
-        console.log(response); // Check the response structure
-        setData(response.data); // Set the state with response.data
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://10.0.142.62:8000/news/tim%20cook")
+  //     .then((response) => {
+  //       console.log(response); // Check the response structure
+  //       setData(response.data); // Set the state with response.data
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
   return (
     <main className="">
       <button>test</button>
-      {data1?.map((item, index) => (
+      {/* {data1?.map((item, index) => (
         <p key={index}>{item.title}</p>
-      ))}
+      ))} */}
 
     </main>
   );
