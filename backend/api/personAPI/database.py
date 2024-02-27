483cb874e41bf27f59e21ed586ce90c2a01352e0
ytgoh2021 <112225704+ytgoh2021@users.noreply.github.com>
2024-02-27 14:46:10 +0800

    Bug fix for database connectivity

--- backend/api/personAPI/database.py
+++ backend/api/personAPI/database.py
@@ -4,11 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-if os.getenv("DB_KEY"):
-    SQLALCHEMY_DATABASE_URL = (
-        "mysql+pymysql://root:" + os.getenv("DB_KEY") + "@localhost:3306/CNS-T3"
-    )
+SQLALCHEMY_DATABASE_URL = os.getenv("AZURE_SQL_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
