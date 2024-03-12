8857aa9ad34863a447bcc1b46df3a5ee99702682
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-03-12 23:21:43 +0800

    feature/SCRUM-47-Create-Configuration-File-for-News-Categories (#24)
    * Created json file
    
    * Finished category config functionality
    
    * Fixed lint-fastapi error

--- backend/api/newsAPI/fetch_news.py
+++ backend/api/newsAPI/fetch_news.py
@@ -11,6 +11,26 @@ load_dotenv()
 if os.getenv("OPENAI_API_KEY"):
     openAI_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+def get_categories():
+    with open('backend/config/categories.json', 'r') as file:
+        data = json.load(file)
+    formatted_categories = [category.title() for category in data['categories']]
+
+    final_string = ', '.join(formatted_categories)
+    return final_string
+
+def get_prompt():
+    with open("backend/api/newsAPI/prompt.txt", "r") as file:
+        data = file.read()
+    return data
+
+
+def get_search_patterns():
+    with open('backend/config/categories.json', 'r') as file:
+        data = json.load(file)
+    formatted_string = ', '.join([f"{category} - search pattern: {', '.join(patterns)}" for category, patterns in data['categories'].items() if patterns])
+    return formatted_string
+
 
 def get_summarised_news_articles(search_query: str):
     """
@@ -83,47 +103,19 @@ def get_summarised_news_articles(search_query: str):
 
 def summarise_article(article_body, client):
     input = "Summarise this article for me: " + article_body
+    prompt = get_prompt()
+    categories = get_categories()
+    search_patterns = get_search_patterns()
+    prompt = prompt.replace("{{ categories }}", categories)
+    prompt = prompt.replace("{{ search_patterns }}", search_patterns)
+
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
         response_format={"type": "json_object"},
         messages=[
             {
                 "role": "system",
-                "content": """Given a news article, complete the following actions
-
-1. Summarize the news content in less than 100 words
-
-2. Provide a category(ies) for the article (Source of Wealth, Family Circumstances, Sanctioned Countries, Sensitive industries). If none of the categories provided fits the article’s content, return Others as the category. It should only be one of these 5 categories: Source of Wealth, Family Circumstances, Sanctioned Countries, Sensitive industries, Others.
-- To determine the category, these are the search patterns for each category:
-- Source of wealth – search patterns : net worth, worth, income, inherit, inheritance, receive, gifted, settlement, sale proceeds, merger & acquisition, M&A, leveraged buyout
-- Family circumstances – search patterns:  Marry, marriage, divorce, separation, children, child
-- Sanctioned countries – search patterns (limited set): Cuba, Iran, North Korea, Syria, Crimea, Russia, Ukraine, Afghanistan, Albania, Algeria, Angola, Bahamas, Bangladesh, Barbados, Belarus, Belize, Bolivia, Botswana, Burkina Faso, Burundi, Cambodia, Cameroon, Central African Republic, Chad, Cayman Islands, Comoros, Congo, Democratic Republic of Côte d'Ivoire, Djibouti, Dominican Republic, Ecuador, Egypt
-- Sensitive industries – search patterns: Casinos, Arms Dealer, Arms Material, Private Military Service Provider, Diamond, Precious stones, Money Remittance Business 
-
-It should only be one of these 5 categories: Source of Wealth, Family Circumstances, Sanctioned Countries, Sensitive industries, Others.
-
-3. Determine whether the article focuses on an individual engaging in the action rather than their associated company, and provide a true or false response. For example, ascertain if the article is about Tim Cook investing personally, not Apple as a company.
-
-4. Give me a risk rating (low, medium or high) based on the following cases:
-High Risk Rating Cases:
-- Legal Issues: Scandals or legal troubles involving the client, News about the client being involved in fraud or money laundering investigations.
-- Financial Distress: Reports of the client facing bankruptcy or financial instability, Significant debt or default issues.
-- Sanctions and Embargoes: News indicating that the client or their associated entities are subject to sanctions or embargoes, News indicating that the client is investing in a sanctioned country (given above).
-- Politically Exposed Persons (PEP): Identification of the client as a PEP, News about the client's involvement with politically sensitive activities.
-Industry-specific Risks: Regulatory violations or controversies within the client's industry, Any news indicating non-compliance with industry standards.
-
-Medium-Risk Cases:
-- Change in Business Activities: Reports of significant changes in the client's business model or activities.
-- Mergers and Acquisitions: News about the client being involved in mergers, acquisitions, or significant corporate restructuring.
-- Market Performance: Fluctuations in the client's stock prices or financial performance.
-- Management Changes: Significant changes in the client's leadership or management team.
-- Partnerships and Alliances: News about the client forming partnerships or alliances that may pose moderate risks.
-
-If the news does not belong to any of the above cases, return Low for its risk rating.
-
-5. Identify the subject in the article, be it an entity or person. Generate a profile summary of the subject based on the information from the article in less than 100 words.
-
-Return the data in JSON format: {"summary": "", "category": "", "is_related": "", "risk_rating":"", "subject_summary": ""}""",
+                "content": prompt,
             },
             {"role": "user", "content": input},
         ],
