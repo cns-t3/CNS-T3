7a6da289d5c1da05aca718c8e31729bf029f0b35
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-03-15 01:09:46 +0800

    Updated host to clusterIP

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 async function getData() {
-  const url = 'http://127.0.0.1:8002/news/anthony%20tan';
+  const url = 'http://10.0.114.249:8002/news/anthony%20tan';
   try {
     const res = await fetch(url, { cache: 'no-store' });
     if (!res.ok) {
