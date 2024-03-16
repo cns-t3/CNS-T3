0de3230df0daf148d592de5d86c7acf85b689a3d
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-03-16 20:52:26 +0800

    Test person deployment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ async function getData() {
   }
 }
 async function getPerson(name) {
-  const personHostname = '0.0.0.0'; // Assuming you want to use a specific hostname
+  const personHostname = '10.0.36.137'; // Assuming you want to use a specific hostname
   const url = `http://${personHostname}:8001/persons/search`;
 
   try {
