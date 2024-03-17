ee8f40ed4c26a250cd928f08f9e1238990cebe10
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-03-17 22:38:38 +0800

    Test person deployment

--- backend/api/personAPI/pydantic_models.py
+++ backend/api/personAPI/pydantic_models.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel
 
 class PersonSchema(BaseModel):
-    person_id: int | None
-    name: str | None
-    occupation: str | None
-    dob: str | None
-    nationality: str | None
-    description: str | None
-    company: str | None
-    country_of_residency: str | None
-    pep_status: bool | None
-    source_of_wealth: str | None
-    img_url: str | None
+    person_id: int
+    name: str
+    occupation: str
+    dob: str
+    nationality: str
+    description: str
+    company: str
+    country_of_residency: str
+    pep_status: bool
+    source_of_wealth: str
+    img_url: str
