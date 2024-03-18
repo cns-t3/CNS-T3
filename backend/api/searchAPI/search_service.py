96bdb0fb5143c62559fbaf0bb824462d73669058
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-03-18 17:44:53 +0800

    feature/SCRUM-31-Set-up-CD-pipeline-updated (#29)
    * Test docker images
    
    * Test aks deployment for news and frontend
    
    * Test workflow
    
    * Test secrets in kubernetes
    
    * Updated host to clusterIP
    
    * Deploy rest of the services
    
    * Added dns communication
    
    * Fixed fastapi lint error
    
    * Test workflow run
    
    * Test workflow
    
    * Test workflow
    
    * Test DNS in frontend
    
    * Text nextjs env retrieval
    
    * Test nextjs env retrieval
    
    * Test nextjs env retrieval
    
    * Deploy person service
    
    * Test env variables
    
    * Changed person deployment
    
    * Test person deployment
    
    * Test person deployment
    
    * Test Person deployment
    
    * Test search deployment
    
    * Test identity deployment
    
    * Fixed typo
    
    * Test deployments
    
    * Change dns
    
    * Added missing numpy requirements
    
    * Test nextjs env retrieval
    
    * Test workflow run
    
    * Test workflow
    
    * Test workflow
    
    * Test workflow
    
    * Update search deployment
    
    * Test workflow
    
    * Test workflow
    
    * Test deployment
    
    * Fixed error
    
    * Fixed news sources json error
    
    * Change requirement for pipeline

--- backend/api/searchAPI/search_service.py
+++ backend/api/searchAPI/search_service.py
@@ -4,12 +4,31 @@ from fastapi import HTTPException
 from backend.api.searchAPI.pydantic_models import SearchResult
 from backend.api.searchAPI.azure_service import download_from_azure
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+if os.getenv("PERSON_DNS"):
+    person_hostname = os.getenv("PERSON_DNS")
+else:
+    person_hostname = "127.0.0.1"
+
+if os.getenv("NEWS_DNS"):
+    news_hostname = os.getenv("NEWS_DNS")
+else:
+    news_hostname = "127.0.0.1"
+
+if os.getenv("IDENTITY_DNS"):
+    identity_hostname = os.getenv("IDENTITY_DNS")
+else:
+    identity_hostname = "127.0.0.1"
 
 def search_person_service(search_query: str):
     params = {"name": search_query}
 
     # Get person's profile
-    person_endpoint = "http://127.0.0.1:8001/persons/search/"
+    person_endpoint = f"http://{person_hostname}:8001/persons/search/"
     response = requests.get(person_endpoint, params=params)
     if response.status_code != 200:
         raise HTTPException(
@@ -21,7 +40,7 @@ def search_person_service(search_query: str):
 
 # To get all persons in the database
 def search_persons_service():
-    person_all_endpoint = "http://127.0.0.1:8001/persons/"
+    person_all_endpoint = "http://{person_hostname}:8001/persons/"
     response = requests.get(person_all_endpoint)
     if response.status_code != 200:
         raise HTTPException(status_code=404, detail="No persons found")
@@ -41,7 +60,7 @@ def get_search_result_from_person(person, daily_job=False):
             print("Not found in cache")
             print(e)
 
-    news_endpoint = "http://127.0.0.1:8002/news/" + person["name"]
+    news_endpoint = "http://{news_hostname}:8002/news/" + person["name"]
     response = requests.get(news_endpoint)
     if response.status_code != 200:
         raise HTTPException(
@@ -52,7 +71,7 @@ def get_search_result_from_person(person, daily_job=False):
     search_result = SearchResult(person=person, newsArticles=news_articles)
 
     # Get identity matching score
-    identity_endpoint = "http://127.0.0.1:8003/identity"
+    identity_endpoint = f"http://{identity_hostname}:8003/identity"
     response = requests.post(
         identity_endpoint, data=(search_result.model_dump_json()).encode("utf-8")
     )
