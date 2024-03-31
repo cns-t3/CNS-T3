4f4152316e11f46e1daf55f7ca5c1003ab5afc22
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-04-01 02:07:50 +0800

    Added nextjs route handler

---
+++ src/app/api/person/route.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/prefer-default-export */
+export async function GET(req) {
+  const { searchParams } = req.nextUrl;
+  const query = searchParams.get('name');
+  try {
+    const personDNS = process.env.NEXT_PUBLIC_PERSON_DNS || '127.0.0.1';
+    const response = await fetch(`http://${personDNS}:8001/persons/search?name=${query}`);
+
+    if (!response.ok) {
+      return new Response('Error getting person data', {
+        status: response.status,
+      });
+    }
+
+    const data = await response.json();
+    return Response.json(data);
+  } catch (error) {
+    return new Response(`Error: ${error.message}`, {
+      status: 400,
+    });
+  }
+}
