142550be8ee53bed7f54952c66725d9d8213cd89
ytgoh2021 <112225704+ytgoh2021@users.noreply.github.com>
2024-04-07 23:37:24 +0800

    Hotfix for daily data fetching
    - Modified f string
    - Modified categories.json filepath

--- backend/api/newsAPI/fetch_news.py
+++ backend/api/newsAPI/fetch_news.py
@@ -33,7 +33,7 @@ def get_prompt():
 
 
 def get_search_patterns():
-    with open("backend/config/categories.json", "r") as file:
+    with open("backend/api/newsAPI/categories.json", "r") as file:
         data = json.load(file)
     formatted_string = ", ".join(
         [
@@ -44,11 +44,13 @@ def get_search_patterns():
     )
     return formatted_string
 
+
 # New function to get reputable news sources from JSON
 def get_reputable_news_sources():
-    with open('backend/api/newsAPI/news_sources.json', 'r') as file:
+    with open("backend/api/newsAPI/news_sources.json", "r") as file:
         data = json.load(file)
-    return data['sources']
+    return data["sources"]
+
 
 def get_summarised_news_articles(search_query: str):
     """
@@ -59,7 +61,7 @@ def get_summarised_news_articles(search_query: str):
         + os.getenv("NEWS_API_KEY")
         + "&keyword="
         + search_query
-        + "&lang=eng&articlesSortBy=rel&articlesCount=10&isDuplicateFilter=skipDuplicates"
+        + "&lang=eng&articlesSortBy=rel&articlesCount=20&isDuplicateFilter=skipDuplicates&dateStart=2023-01-01&dateEnd=2024-04-01"
     )
     reputable_sources = get_reputable_news_sources()  # Get reputable sources
 
@@ -69,7 +71,11 @@ def get_summarised_news_articles(search_query: str):
         if response.status_code == 200:
             articles = response.json()["articles"]["results"]
             # Filter articles by reputable sources
-            articles = [article for article in articles if article["source"]["title"] in reputable_sources]
+            # articles = [
+            #     article
+            # for article in articles
+            # if article["source"]["title"] in reputable_sources
+            # ]
             threads = []
             summaries = [""] * len(articles)
             categories = [""] * len(articles)
