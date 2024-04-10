3b2107798a01bbc53fd688231fe9fc169b67eea6
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2024-04-11 04:26:27 +0800

    Test deployment

--- src/components/Result/ResultsViewer.jsx
+++ src/components/Result/ResultsViewer.jsx
@@ -28,11 +28,11 @@ function ResultsViewer({ data }) {
   };
 
   return (
-    <div className="fixed top-[230px] md:top-[180px] z-0 h-[calc(100vh-230px)] md:h-[calc(100vh-180px)] overflow-y-auto">
+    <div className="fixed top-[230px] md:top-[180px] z-0  md:h-[calc(100vh-180px)] overflow-y-auto">
       <div className="px-10">
         <div className="flex flex-col lg:flex-row w-full">
           <div
-            className={`flex bg-beige rounded-md h-min lg:order-last lg:overflow-y-auto lg:h-[calc(100vh-200px)] w-full ${
+            className={`flex bg-beige rounded-md lg:order-last overflow-y-auto h-[calc(100vh-250px)] lg:h-[calc(100vh-200px)] w-full ${
               articleDetails == null ? 'lg:w-1/3' : 'lg:w-1/2'
             }`}
           >
